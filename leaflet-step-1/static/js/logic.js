4a1216fc8476f4c68cf14aac096430b45a45346d
mlincks3601 <mlincks3601@gmail.com>
2021-07-15 22:30:53 -0400

    new code added

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,70 +1,33 @@
+ 
+ 
+// Creating map object
+var myMap = L.map("map", {
+  center: [31.51, -96.42],
+  zoom: 8,
+  layers: [
+    layers.,
+    layers.,
+    layers.,
+    layers.,
+    layers.
+  ]
+});
+ 
+ 
  // Store our API endpoint
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 // Add a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
-// var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.opens
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "mapbox/streets-v11",
-//   accessToken: API_KEY
-// }).addTo(myMap);
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.opens
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
 // 
-//Create the map with our layers
-// var map = L.map("map", {
-    // center: [40.73, -74.0059],
-    // zoom: 12,
-    // layers: [
-    //   layers.COMING_SOON,
-    //   layers.EMPTY,
-    //   layers.LOW,
-    //   layers.NORMAL,
-    //   layers.OUT_OF_ORDER
-    // ]
-//   });
-//   
-  // Add our 'lightmap' tile layer to the map
-  //lightmap.addTo(map);
-
-
-// create the function that cretes the map and adds the layers to the map
-function createMap(earthquakes) {
  
-    // Define streetmap and darkmap layers
-    var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.outdoors",
-      accessToken: API_KEY
-    });
-  
-    var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.light",
-      accessToken: API_KEY
-    });
- 
-    var grayscale = L.tileLayer.grayscale('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>", 
-     maxZoom: 18,
-     });
-  
-    var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-     maxZoom: 18,
-     id: "mapbox.satellite",
-     accessToken: API_KEY
-   })
- 
-    // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-      "Light Map": light,
-      "Outdoors": outdoors,
-      "Satellite" : satellite,
-      "Grayscale" : grayscale
-    };
   
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
@@ -90,17 +53,3 @@ function createMap(earthquakes) {
     legend.addTo(myMap);
  
   }
-  
- © 2021 GitHub, Inc.
- Terms
- Privacy
- Security
- Status
- Docs
- Contact GitHub
- Pricing
- API
- Training
- Blog
- About
- 
